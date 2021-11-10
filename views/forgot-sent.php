5a0b3bd8a19c05c949cd5f96f3e8eed89a2ec97a
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-11-10 15:29:29 -0400

    update code

--- views/forgot-sent.php
+++ views/forgot-sent.php
@@ -1,29 +1,48 @@
-<div class="product-big-title-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="product-bit-title text-center">
-                    <h2>Esqueceu a Senha?</h2>
+<?php
+
+$User = new User();
+$User->verifyExistLoginUser();
+
+$Component = new Component();
+echo $Component->getHtmlDashboard();
+echo $Component->getMenuAndSideBarDashboard();
+
+?>
+
+
+    <div class="product-big-title-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-bit-title text-center">
+                        <h2>Esqueceu a Senha?</h2>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="single-product-area">
-    <div class="zigzag-bottom"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="alert alert-success" role="alert">
-                    <h4 class="alert-heading">E-mail enviado!</h4>
-                    <p>Verifique seu e-mail e siga as instruções para recuperar a sua senha.</p>
-                </div>                  
+    <div class="single-product-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="alert alert-success" role="alert">
+                        <h4 class="alert-heading">E-mail enviado!</h4>
+                        <p>Verifique seu e-mail e siga as instruções para recuperar a sua senha.</p>
+                    </div>                  
+                </div>
             </div>
         </div>
     </div>
-</div>
 
+
+<?php
+
+$Component = new Component();
+echo $Component->getFooterDashboard();
+
+?>
 
 
 
