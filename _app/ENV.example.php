c3164694a0b7185fd2ae67dc73ec4a691e3f0615
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-11-22 18:16:56 -0400

    Variaveis de Ambiente

---
+++ _app/ENV.example.php
@@ -0,0 +1,15 @@
+<?php
+const HOST = '';
+const USER = '';
+const PASS = '';
+const DBSA = '';
+
+const COMPANY_EMAIL = '';
+const COMPANY_NAME = '';
+
+const EMAIL_HOST = '';
+const EMAIL_USER = '';
+const EMAIL_PASS = '';
+const EMAIL_PORT = '';
+
+const BASE = '';
