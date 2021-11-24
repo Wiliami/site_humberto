79ec3ae38bd4499933cf268037aa25106b71461c
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-11-24 08:49:59 -0400

    adicionando codigo na views biografia

--- views/biografia.php
+++ views/biografia.php
@@ -7,22 +7,49 @@
 
 ?>
 
-    <header class="header-2" style="height: 698px;">
-            <div class="page-header min-vh-100 relative" style="background-image: url('<?= BASE ?>/src/images/page-youtube.jpg')">
-                <span class="mask bg-gradient-primary opacity-4"></span>
-                <div class="container">
-                    <div class="row">
-                        <div class="my-5 text-center text-xl-start">
-                            <h1 class="display-5 fw-bolder text-black mb-2">Biografia</h1>
-                            <p class="lead text-black mt-3">Ministério Pastoral de Humberto Oliveira</p>
+            <section class="py-5">
+                <div class="container px-5">
+                    <div class="row gx-5 align-items-center justify-content-center">
+                        <div class="col-lg-8 col-xl-7 col-xxl-6">
+                            <div class="my-5 text-center text-xl-start">
+
+                            <h1 class="mb-0">Pr. Humberto Oliveira</h1>
+                            <div class="col-sm-9" style="margin">
+                                <p class="mb-0">
+                                No último sábado, 30 de outubro, a liderança da Igreja Adventista da região sudoeste 
+                                paulista, realizou a cerimônia de ordenação ao Ministério Pastoral dos pastores Thiago da
+                                Silva Monteiro e Joel Gomes de Sousa. O evento aconteceu na Igreja Central de Sorocaba e 
+                                contou com a presença de líderes paulista da Igreja Adventista, parentes, amigos, pastores e
+                                membros de outros distritos.
+                                </p><br>
+
+                                <p class="mb-0">
+                                A mensagem direcionada para os pastores foi do líder da Igreja Adventista para todo Estado
+                                de São Paulo, o pastor Maurício Lima. Nas suas considerações ele ressaltou algumas marcas
+                                que devem fazer parte do ministério do pastor. “É muito importante a comunhão diária
+                                com Deus, a marca da espiritualidade. A marca da justiça e a marca do amor. Entre outras marcas, 
+                                essas são indispensáveis”, ressaltou Lima.
+                                </p><br>
+
+                                <p class="mb-0">
+                                O pastor Josemar Ventura é o responsável pelo grupo de 75 pastores no território sudoeste 
+                                paulista. Para ele, participar desse momento na vida do pastor é muito significativo. 
+                                “Realmente a gente sente uma alegria ao perceber que o Espírito Santo convence corações 
+                                de jovens para se dedicarem, de forma integral, no trabalho de Deus e na salvação de pessoas.”
+                                </p><br>
+                            </div>
+
+                            </div>
                         </div>
-                        <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                            <a class="btn btn-danger btn-lg px-4 me-sm-3" href="https://www.youtube.com/channel/UClFMwemd5j7EsXlV-gTw5Xg">INSCREVA-SE</a>
-                        </div>  
+
+                        <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+                            <img class="img-fluid rounded-3 my-5 bg-warning rounded-circle w-290 h-290" src="<?= BASE ?>/src/images/humberto.png" alt="image de Humberto Oliveira" />
+                        </div>
+
+
                     </div>
                 </div>
-            </div>
-    </header>
+            </section>
 
 
 <?php 
