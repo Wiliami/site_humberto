5ce7314725ad89a187ba1de252639a5cbaf63360
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-11-26 16:09:49 -0400

    quase terminando o front

--- views/login.php
+++ views/login.php
@@ -1,16 +1,15 @@
 <?php
 
-$User = new User();
-if($User->verifyLoginUserON()) {
-    header('Location: ' . BASE . '/dashboard');
-    die();
-
+    $User = new User();
+    if($User->verifyLoginUserON()) {
+        header('Location: ' . BASE . '/dashboard');
+        die();
 };
 
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,28 +17,57 @@ if($User->verifyLoginUserON()) {
         <title>Página | Login</title>
         
         <!-- Google Fonts -->
-        <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+        <!-- <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'> -->
         
         <!-- Bootstrap -->  
-        <link rel="stylesheet" href="<?= BASE ?>/res/site/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="<?= BASE ?>/res/site/css/bootstrap.min.css"> -->
         
         <!-- Font Awesome -->   
-        <link rel="stylesheet" href="<?= BASE ?>/res/site/css/font-awesome.min.css">
+        <!-- <link rel="stylesheet" href="<?= BASE ?>/res/site/css/font-awesome.min.css"> -->
         
         <!-- Custom CSS -->
         <!-- <link rel="stylesheet" href="<?= BASE ?>/res/site/css/owl.carousel.css"> -->
-        <link rel="stylesheet" href="<?= BASE ?>/res/site/css/style.css">
-        <link rel="stylesheet" href="<?= BASE ?>/res/site/css/responsive.css">
+        <!-- <link rel="stylesheet" href="<?= BASE ?>/res/site/css/style.css">
+        <link rel="stylesheet" href="<?= BASE ?>/res/site/css/responsive.css"> -->
 
-
-    </head>
-    <body>
-
+        <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
 
 
 
+        <link id="pagestyle" href="<?= BASE ?>/src/css/material-kit.css?v=3.0.0" rel="stylesheet" />
+        <link href="<?= BASE ?>/res/site/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="<?= BASE ?>/res/site/css/paper-kit.css?v=2.2.0" rel="stylesheet" /> -->
+
+<!DOCTYPE html>
+     <html lang="pt-BR">
+        <head>
+            <meta charset="utf-8" />
+            <link rel="apple-touch-icon" sizes="76x76" href="./assets/img//apple-icon.png">
+            <link rel="icon" type="image/png" href="./assets/img//favicon.png">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+            <title>
+                Paper Kit by Creative Tim
+            </title>
+            <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+
+            <!-- Fonts and icons -->
+            <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+            <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+            
+            <link id="pagestyle" href="<?= BASE ?>/src/css/material-kit.css?v=3.0.0" rel="stylesheet" />
+            
+            <!-- CSS Files -->
+            <link href="<?= BASE ?>/res/site/css/bootstrap.min.css" rel="stylesheet" />
+            <link href="<?= BASE ?>/res/site/css/paper-kit.css?v=2.2.0" rel="stylesheet" />
+          
+
+
+        </head>
+
+        <body class="index-page sidebar-collapse">
 
         <!-- <div class="single-product-area">
             <div class="zigzag-bottom"></div>
@@ -99,58 +127,36 @@ if($User->verifyLoginUserON()) {
         </div> -->
 
 
-
-            <div class="section section-image section-login" style="background-image: url('./assets/img/login-image.jpg');">
+            <div class="section section-image section-login" style="background-image: url('<?= BASE ?>/src/images/login-image.jpg')">
             <div class="container">
                 <div class="row">
                 <div class="col-lg-4 col-md-6 mx-auto">
                     <div class="card card-register">
-                    <h3 class="title mx-auto">Welcome</h3>
-                    <div class="social-line text-center">
-                        <a href="#pablo" class="btn btn-neutral btn-facebook btn-just-icon mt-0">
-                        <i class="fa fa-facebook-square"></i>
-                        </a>
-                        <a href="#pablo" class="btn btn-neutral btn-google btn-just-icon mt-0">
-                        <i class="fa fa-google-plus"></i>
-                        </a>
-                        <a href="#pablo" class="btn btn-neutral btn-twitter btn-just-icon mt-0">
-                        <i class="fa fa-twitter"></i>
-                        </a>
-                    </div>
+                    <h3 class="title mx-auto">Seja bem-vindo(a)</h3>
+                
                     <form class="register-form">
                         <label>Email</label>
                         <div class="input-group form-group-no-border">
                         <div class="input-group-prepend">
-                            <span class="input-group-text">
-                            <i class="nc-icon nc-email-85"></i>
-                            </span>
                         </div>
-                        <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control" name="email" placeholder="Email">
                         </div>
-                        <label>Password</label>
+                        <label>Senha</label>
                         <div class="input-group form-group-no-border">
                         <div class="input-group-prepend">
-                            <span class="input-group-text">
-                            <i class="nc-icon nc-key-25"></i>
-                            </span>
                         </div>
-                        <input type="password" class="form-control" placeholder="Password">
+                            <input type="password" class="form-control" name="password" placeholder="Senha">
                         </div>
-                        <button class="btn btn-danger btn-block btn-round">Register</button>
+                        <button class="btn btn-danger btn-block btn-round" name="login">Entrar</button>
                     </form>
                     <div class="forgot">
-                        <a href="#" class="btn btn-link btn-danger">Forgot password?</a>
+                        <a href="<?= BASE ?>/forgot-password" class="btn btn-link btn-danger">Esqueci a senha</a>
                     </div>
-                    </div>
-                    <div class="col text-center">
-                    <a href="./examples/register-page.html" class="btn btn-outline-neutral btn-round btn-lg" target="_blank">View Register Page</a>
                     </div>
                 </div>
                 </div>
             </div>
         </div>
-        
-
     </body>
 </html>
 
