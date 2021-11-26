0a448c20039e83940840fa1fcbf7ea9dc42ac0bd
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-11-26 11:16:16 -0400

    tentandoo terminar o front

--- views/index.php
+++ views/index.php
@@ -22,9 +22,6 @@
 
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-
-
-
     
     <!-- Estiliza as letras do card cursos -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
@@ -159,51 +156,7 @@
 
 
             <!-- carrosel -->
-            <div class="section pt-o" id="carousel">
-            <div class="container">
-                <div class="row">
-                <div class="col-md-8 ml-auto mr-auto">
-                    <div class="card page-carousel">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active">
-                            <img class="d-block img-fluid" src="<?= BASE ?>/src/images/humberto-background-bio.jpeg" alt="First slide">
-                            <div class="carousel-caption d-none d-md-block">
-                            <p>Somewhere</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block img-fluid" src="<?= BASE ?>/src/images/humberto-background-carrousel.jpeg" alt="Second slide">
-                            <div class="carousel-caption d-none d-md-block">
-                            <p>Somewhere else</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block img-fluid" src="<?= BASE ?>/src/images/humberto-background-bio.jpeg" alt="Third slide">
-                            <div class="carousel-caption d-none d-md-block">
-                            <p>Here it is</p>
-                            </div>
-                        </div>
-                        </div>
-                        <a class="left carousel-control carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="fa fa-angle-left"></span>
-                        <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="fa fa-angle-right"></span>
-                        <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            </div>
+            
 
 
 
--- views/login.php
+++ views/login.php
@@ -29,26 +29,15 @@ if($User->verifyLoginUserON()) {
         <link rel="stylesheet" href="<?= BASE ?>/res/site/css/font-awesome.min.css">
         
         <!-- Custom CSS -->
-        <link rel="stylesheet" href="<?= BASE ?>/res/site/css/owl.carousel.css">
+        <!-- <link rel="stylesheet" href="<?= BASE ?>/res/site/css/owl.carousel.css"> -->
         <link rel="stylesheet" href="<?= BASE ?>/res/site/css/style.css">
         <link rel="stylesheet" href="<?= BASE ?>/res/site/css/responsive.css">
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
 
 
     </head>
     <body>
 
 
-        <!-- <?php
-        // $Component = new Component();
-        // echo $Component->getMenu();
-        ?> -->
 
 
 
@@ -107,7 +96,7 @@ if($User->verifyLoginUserON()) {
                 </div>
             </div>
         </div>
-    
+        
 
     </body>
 </html>
