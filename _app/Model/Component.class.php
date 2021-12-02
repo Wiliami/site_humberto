1939a1fa1992fc433a32742bcf37383274c23eeb
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-02 08:18:56 -0400

    views

--- _app/Model/Component.class.php
+++ _app/Model/Component.class.php
@@ -266,8 +266,11 @@ class Component {
                     </div>
                 </li>
 
-                <!-- Nav Item - Utilities Collapse Menu -->
-                <li class="nav-item">
+                <!-- Nav Item - Pages Collapse Menu -->
+                <?php 
+                    if( '. $_SESSION["login"]["user_level"] >= 1 . ') {
+                        ?>
+                            <li class="nav-item">
                                 <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#collapsePages"
                                     aria-expanded="true" aria-controls="collapsePages">
                                     <i class="fas fa-fw fa-folder"></i>
@@ -276,16 +279,18 @@ class Component {
 
                                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                                     <div class="bg-white py-2 collapse-inner rounded">
-                                        <a class="collapse-item" href="'. BASE .'/list-user">Lista de usuários</a>
-                                        <a class="collapse-item" href="'. BASE .'/cursos-aprovacao">Cursos em análise</a>
-                                        <a class="collapse-item" href="'. BASE .'/historico-compras">Histórico de compras</a>
-                                        <a class="collapse-item" href="'. BASE .'/suporte">Suporte</a>
-                                        <a class="collapse-item" href="'. BASE .'/settings">Configurações</a>
-                                        
+                                        <a class="collapse-item" href="'.  BASE .'/list-user">Lista de usuários</a>
+                                        <a class="collapse-item" href="'.  BASE .'/cursos-aprovacao">Cursos em análise</a>
+                                        <a class="collapse-item" href="'.  BASE .'/historico-compras">Histórico de compras</a>
+                                        <a class="collapse-item" href="'.  BASE .'/suporte">Suporte</a>
+                                        <a class="collapse-item" href="'.  BASE .'/settings">Configurações</a>
                                     </div>
                                 </div>
 
                             </li>
+                        
+                <?php
+                }?>
 
 
                 <!-- Divider -->
--- views/dashboard.php
+++ views/dashboard.php
@@ -1,47 +1,18 @@
-
-
-                <!-- Nav Item - Pages Collapse Menu -->
-                <!-- <?php 
-                    if($_SESSION['login']['user_level'] >= 1) {
-                        ?>
-                            <li class="nav-item">
-                                <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#collapsePages"
-                                    aria-expanded="true" aria-controls="collapsePages">
-                                    <i class="fas fa-fw fa-folder"></i>
-                                    <span>Administrativo</span>
-                                </a>
-
-                                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                                    <div class="bg-white py-2 collapse-inner rounded">
-                                        <a class="collapse-item" href="<?= BASE ?>/list-user">Lista de usuários</a>
-                                        <a class="collapse-item" href="<?= BASE ?>/cursos-aprovacao">Cursos em análise</a>
-                                        <a class="collapse-item" href="<?= BASE ?>/historico-compras">Histórico de compras</a>
-                                        <a class="collapse-item" href="<?= BASE ?>/suporte">Suporte</a>
-                                        <a class="collapse-item" href="<?= BASE ?>/settings">Configurações</a>
-                                    </div>
-                                </div>
-
-                            </li>
-                        
-                <?php
-                }?> -->
-
-               
-
-
-
 <?php
 
 $User = new User();
 $User->verifyExistLoginUser();
 $Component = new Component();
-
 echo $Component->getHeadHtmlPages();
 echo $Component->getMenuAndSideBarDashboard();
 echo $Component->getBarraMenuOptions();
+
 ?>
+
 Configurações
+
 <?php
+
 echo $Component->getFooterDashboard();
 
 ?>
