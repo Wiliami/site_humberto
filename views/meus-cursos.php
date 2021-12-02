9e898fc479eefe74c8f1a3810f69e45dc40b8ef5
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-02 11:35:49 -0400

    first commit of day

--- views/meus-cursos.php
+++ views/meus-cursos.php
@@ -9,16 +9,6 @@
 ?>
 
 
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página | Cursos</title>
-</head>
-<body>
-
     <div style="margin-left: 20px;">
         <h3>Olá, <?= $_SESSION['login']['user_name'] ?></h3>
         <span>Meus cursos</span>
