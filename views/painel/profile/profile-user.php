cbb0195d6168ea45c9073292f453c6b9c9ae318c
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-06 14:41:17 -0400

    atualizando as views

--- views/painel/profile/profile-user.php
+++ views/painel/profile/profile-user.php
@@ -16,14 +16,12 @@
             <label for="inputPassword" class="col-sm-2 col-form-label">Nome</label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" value="<?= $_SESSION['login'] ['user_name'] ?>" id="inputPassword">
-                
             </div>
         </div>
         <div class="form-group row" style="margin-left: 20px;">
             <label for="inputPassword" class="col-sm-2 col-form-label">E-mail</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" value="<?= $_SESSION['login'] ['user_email'] ?>" id="inputPassword">
-                 
+                <input type="text" class="form-control" value="<?= $_SESSION['login'] ['user_email'] ?>" id="inputPassword">     
             </div>
         </div>
         <div class="form-group row" style="margin-left: 20px;">
@@ -32,7 +30,6 @@
                 <input type="text" class="form-control" id="inputPassword">
             </div>
         </div>
-
         <input type="submit" class="btn btn-primary mb-2" value="Continuar" style="margin: 30px;">
     </form>
         
