5865f212ba820d91d197ee1db60744185dacf7b7
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-06 15:38:24 -0400

    atualizando as views

--- views/painel/aulas.php
+++ views/painel/aulas.php
@@ -244,7 +244,7 @@
                                 <!-- Dropdown - User Information -->
                                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                     aria-labelledby="userDropdown">
-                                    <a class="dropdown-item" href="<?= BASE ?>/painel/profile/profile">
+                                    <a class="dropdown-item" href="<?= BASE ?>/painel/profile/profile-user">
                                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Meus dados
                                     </a>
