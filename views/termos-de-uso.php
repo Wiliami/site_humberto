81dd851f9cbb07f82902a39a1c3d5de1b36c8d30
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-16 10:43:38 -0400

    terminando os ultimos ajustes das paginas

--- views/termos-de-uso.php
+++ views/termos-de-uso.php
@@ -19,7 +19,6 @@ echo $Component->getMenu();
                 Estes termos e condições descrevem as regras de uso do site da empresa Humberto Oliveira, localizada no endereço https://www.humbertooliveira.net.
                 Ao acessar este site, consideramos que você está de acordo com os termos e condições abaixo. Não continue a usar o Humberto Oliveira caso você discorde dos termos e condições descritos neste contrato.
                 </p>
-
                 <div class="d-flex justify-content-between align-items-center mb-2">
                     <h5 class="mb-0">Cookies:</h5>
                 </div>
@@ -28,7 +27,6 @@ echo $Component->getMenu();
                 Cookies são arquivos de texto inseridos no seu disco rígido por um servidor de página web. Os cookies não têm permissão para executar programas ou transferir vírus para seu computador. Os cookies são associados exclusivamente a você e só podem ser lidos pelo servidor web do domínio que emitiu o cookie.
                 Nós podemos usar cookies para coletar, armazenar ou rastrear informações para finalidades estatísticas e mercadológicas do nosso site. Você tem a opção de aceitar ou rejeitar os cookies opcionais. Existem alguns cookies obrigatórios, que são necessários para o funcionamento de nosso site. Esses cookies obrigatórios são requerem seu consentimento. Por favor tenha em mente que, ao aceitar os cookies obrigatórios, você também estará aceitando cookies de terceiros, que podem ser usados por terceiros caso você utilize serviços fornecidos por eles em nosso site – por exemplo, uma janela de reprodução de vídeo fornecida por empresas terceiras e integrada ao nosso site. Não continue a usar o Humberto Oliveira caso você discorde dos termos e condições descritos neste contrato.
                 </p>
-
                 <div class="d-flex justify-content-between align-items-center mb-2">
                     <h5 class="mb-0">Licença:</h5>
                 </div>
@@ -40,21 +38,15 @@ echo $Component->getMenu();
                 <li>Reproduzir, duplicar ou copiar materiais do Humberto Oliveira</li>
                 <li>Redistribuir conteúdos do Humberto Oliveira</li>
                 Este Acordo terá efeito a partir da data presente.
-
                 Partes deste site oferecem ao usuário a oportunidade de postar e discutir opiniões e informações em determinadas áreas. Humberto Oliveira não filtra, edita, publica ou revisa Comentários antes que eles sejam apresentados no site. Comentários refletem as opiniões da pessoa que os posta. Na extensão em que as leis aplicáveis permitem, Humberto Oliveira não se responsabiliza legalmente pelos Comentários ou quaisquer danos, riscos ou despesas causadas ou sofridas como resultado do uso, e/ou postagem e/ou aparência dos Comentários deste site.
-
                 Humberto Oliveira reserva para si o direito de recomer quaisquer Comentários que possa ser considerado inapropriado, ofensivo ou quebre os Termos e Condições deste contrato.
-
                 Você declara e garante que:
                 <li>Você tem o direito de postar Comentários em nosso site e tem todas as licenças e consentimentos para tal;</li>
                 <li>Os Comentários não invadem qualquer propriedade intelectual, incluindo direitos autorais, patentes ou marcas registradas de terceiros;</li>
                 <li>Os Comentários não contém material difamatório, injurioso, ofensivo, indecente ou de alguma forma ilícito, que é invasão de privacidade.</li>
                 <li>Os Comentários não serão usados para solicitar ou promover negócios ou apresentar atividades comerciais ou atividades ilícitas.</li>
-
                 Você por meio deste concede a Humberto Oliveira a licença não-exclusiva de usar, reproduzir, editar e autorizar outros a usar, reproduzir ou editar qualquer um de seus Comentários em qualquer e todas as formas, formatos e mídias.
-
                 </p>
-
                 <div class="d-flex justify-content-between align-items-center mb-2">
                     <h5 class="mb-0">Criação de links para nosso conteúdo</h5>
                 </div>
@@ -65,11 +57,8 @@ echo $Component->getMenu();
                 <li>Organizações de mídia;</li>
                 <li>Distribuidores de diretórios online podem inserir links para nosso Site na mesma maneira que inserem hiperlinks para Sites de outras empresas listadas; e</li>
                 <li>Empresas credenciadas, exceto organizações angariantes sem fins lucrativos e grupos de arrecadação de fundos para instituições de caridade, que não podem inserir links para nosso Site sem aprovação prévia.</li>
-
                 Essas organizações podem postar links para nossa página inicial, nossas publicações ou outras informações do Site, contanto que o link: (a) não seja, de forma alguma, enganoso; (b) não insinue falsamente a existência de uma relação de patrocínio, endosso, ou aprovação nossa a produtos e/ou serviços; e (c) seja apropriado ao contexto em que está sendo inserido.
-
                 Nós podemos considerar e aprovar solicitações de link feitas pelos seguintes tipos de organizações:
-
                 <li>fontes de informação bem conhecidas sobre produtos e negócios;</li>
                 <li>sites comunitários ponto.com;</li>
                 <li>associações e outros grupos que representem isntituições de caridade;</li>
@@ -77,28 +66,21 @@ echo $Component->getMenu();
                 <li>portais de internet;</li>
                 <li>firmas de contabilidade, advocacia e consultoria; e</li>
                 <li>instituições educacionais e associações de classe.</li>
-
                 Nós vamos aprovar solicitações de link feitas pelos tipos de organização listados acima se julgarmos que: (a) o link não é desfavorável para nossa imagem e/ou para a imagem de empresas credenciadas; (b) a organização solicitante não possui histórico negativo conosco; (c) o benefício que ganhamos com a visibilidade do link compensa a ausência do Humberto Oliveira; e (d) o link será inserido em um contexto geral informativo. <br/>
-
                 Essas organizações podem postar links para nossa página inicial contanto que o link: (a) não seja, de forma alguma, enganoso; (b) não insinue falsamente a existência de uma relação de patrocínio, endosso, ou aprovação de produtos e/ou serviços da nossa parte; e (c) seja apropriado ao contexto em que está sendo inserido. <br/>
-
                 Se você representa uma das organizações listadas no parágrafo 2 acima e está interessado em postar um link para nosso site, você deve nos informar de seu interesse enviando um e-mail para Humberto Oliveira. Por favor, inclua seu some, o nome da sua organização e suas informações de contato, assim como a URL do seu site, uma lista das URLs do nosso site que você pretende usar como links, e uma lista dos sites nos quais você pretende publicar nossas URLs. Aguarde entre 2 e 3 semanas para receber uma resposta. <br/>
-
                 Organizações aprovadas poderão publicar links para nosso Site das seguintes formas:
                 <li>Usando nosso nome corporativo; ou</li>
                 <li>Usando a URL para onde o link redireciona; ou</li>
                 <li>Usando qualquer outra descrição do nosso Site que faça sentido dentro do contexto e do formato do conteúdo onde o link está sendo inserido.</li>
-
                 Não será permitido o uso ou a publicação de links para o logo e/ou outros elementos visuais da Humberto Oliveira sem um acordo de licença para o uso da marca registrada.
                 </p>
-
                 <div class="d-flex justify-content-between align-items-center mb-2">
                     <h5 class="mb-0">Responsabilização pelo Conteúdo:</h5>
                 </div>
                 <p class="text-lg mb-0"> 
                 Não seremos responsabilizados legalmente por qualquer conteúdo que apareça em nosso Site. Você concorda em nos proteger e nos defender contra todas as acusações que forem levantadas contra nosso Site. Nenhum link deve aparecer em qualquer Site que possa ser interpretado como difamatório, obsceno, criminoso, ou que infrinja, viole ou defenda a violação dos direitos de terceiros.
                 </p>
-
                 <div class="d-flex justify-content-between align-items-center mb-2">
                     <h5 class="mb-0">Remoção de links postados em nosso site:</h5>
                 </div>
@@ -106,7 +88,6 @@ echo $Component->getMenu();
                 Se você encontrar qualquer link em nosso Site que seja de qualquer forma ofensivo, você tem a liberdade de entrar em contato conosco e nos informar do problema a qualquer momento. Vamos considerar as solicitações de remoção de links, mas não somos obrigados a remover quaisquer links do nosso site nem a responder diretamente à sua solicitação. <br/>
                 Nós não garantimos que as informações contidas neste site são corretas. Nós não garantimos integralidade ou exatidão do conteúdo. Não garantimos que o site se manterá disponível ou que o material do site se manterá atualizado.
                 </p>
-
                 <div class="d-flex justify-content-between align-items-center mb-2">
                     <h5 class="mb-0">Declaração de Isenção de Responsabilidade:</h5>
                 </div>
@@ -127,3 +108,5 @@ echo $Component->getMenu();
 <?php 
 echo $Component->getFooterHome();
 ?>
+    </body>
+</html>
