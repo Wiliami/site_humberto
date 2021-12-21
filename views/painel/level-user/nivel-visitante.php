5bc9ef64fe42a71671fecf4772667e6817c037dd
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-21 14:13:50 -0400

    criando a pasta de niveis de usuarios no sistema

--- views/painel/level-user/nivel-visitante.php
+++ views/painel/level-user/nivel-visitante.php
@@ -2,7 +2,7 @@
 $User = new User();
 $User->verifyExistLoginUser();
 $Component = new Component();
-echo $Component->getMenuAndSideBarDashboard2();
+echo $Component->getMenuAndSideBarDashboard();
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -21,50 +21,46 @@ echo $Component->getMenuAndSideBarDashboard2();
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                <div class="main-box clearfix">
-                    <div class="table-responsive">
-                        <table class="table user-list">
-                        <h2>Usuários - Visitantes</h2>
-                            <thead>
-                                <tr>
-                                    <th><span>Usuário</span></th>
-                                    <th><span>Nível Usuário</span></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $Read = new Read();
-                                $Read->FullRead("SELECT * FROM users_levels");
-                                if($Read->getResult()) {
-                                    foreach($Read->getResult() as $Level) {
-                                        ?>
-                                <tr>
-                                    <td>
-                                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar" style="width: 50px; height: 50px">
-                                        <a href="/" class="user-link"></a>
-                                    </td>
-                                    <!-- Nível de usuário -->   
-                                    <td>
-                                        <?= $Level['level_desc'] ?>
-                                    </td>
-                                    <td>
-                                        <a href="/" class="table-link" title="Pesquisar usuários desse nível">
-                                            <span class="fa-stack">
-                                                <i class="fas fa-search"></i>
-                                            </span>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php
-                                    }
-                                } else {
-                                    Error("Ainda não existem usuários!");
-                                }   
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>                
-                </div>
+                    <div class="main-box clearfix">
+                        <div class="table-responsive">
+                            <table class="table user-list">
+                                <h2>Usuários - Visitantes</h2>
+                                <thead>
+                                    <tr>
+                                        <th><span>Usuário</span></th>
+                                        <th><span>Nível Usuário</span></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $Read = new Read();
+                                    $Read->FullRead("SELECT * FROM users_levels");
+                                    if($Read->getResult()) {
+                                        foreach($Read->getResult() as $Level) {
+                                            ?>
+                                    <tr>
+                                        <td>
+                                            <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar" style="width: 50px; height: 50px">
+                                            <a href="/" class="user-link"></a>
+                                        </td>
+                                        <td>
+                                            <?php 
+                                                if($Level['level_desc'] == '1') {
+
+                                                }
+                                            ?>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                        }
+                                    } else {
+                                        Error("Ainda não existem usuários!");
+                                    }   
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>                
+                    </div>
                 </div>
             </div>  
         </div>
