5d041d3754283332333a956f3b20b27b3a3c6cfe
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-21 13:24:33 -0400

    criando a pasta de niveis de usuarios no sistema

--- views/painel/courses/delete.php
+++ views/painel/courses/delete.php
@@ -18,71 +18,70 @@ echo $Component->getMenuAndSideBarDashboard2();
         <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     </head>
     <body id="page-top">
-<body> 
-    <div class="container">
-        <div class="col-lg-7">
-            <div class="card-header py-sm-5 py-3">
-                <h2>Excluir cursos</h2>
-                <p class="lead">Excluir cursos</p>
+        <div class="container">
+            <div class="col-lg-7">
+                <div class="card-header py-sm-5 py-3">
+                    <h2>Excluir cursos</h2>
+                    <p class="lead">Excluir cursos</p>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-            <div class="main-box clearfix">
-                <div class="table-responsive">
-                    <table class="table user-list">
-                        <thead>
-                            <tr>
-                                <th><span>Título</span></th>
-                                <th><span>Criado</span></th>
-                                <th class="text-center"><span>Descrição</span></th>
-                                <th><span>Excluir</span></th>
-                                <th>&nbsp;</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $Read = new Read();
-                            $Read->FullRead("SELECT * FROM cursos");
-                            if($Read->getResult()) {
-                                foreach($Read->getResult() as $Cursos) {
-                                    ?>
-                                    <tr>
-                                <td>
-                                    <a href="/" class="user-link"><?= $Cursos['curso_titulo'] ?></a>
-                                    <span class="user-subhead"></span>
-                                </td>
-                                <td>
-                                    <?= $Cursos['curso_create_date'] ?>  
-                                </td>
-                                
-                                <td>
-                                    <a href="/"><?= $Cursos['curso_descricao'] ?></a>
-                                </td>
-                                <td>
-                                    <a href="/" class="table-link danger">
-                                        <span class="fa-stack">
-                                            <i class="fa fa-square fa-stack-2x"></i>
-                                            <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </td>
-                            </tr>
-                            <?php
-                                }
-                            } else {
-                                Error("Ainda não existem usuários!");
-                            }   
-                            ?>
-                        </tbody>
-                    </table>
-                </div>                
+            <div class="row">
+                <div class="col-lg-12">
+                <div class="main-box clearfix">
+                    <div class="table-responsive">
+                        <table class="table user-list">
+                            <thead>
+                                <tr>
+                                    <th><span>Título</span></th>
+                                    <th><span>Criado</span></th>
+                                    <th class="text-center"><span>Descrição</span></th>
+                                    <th><span>Excluir</span></th>
+                                    <th>&nbsp;</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $Read = new Read();
+                                $Read->FullRead("SELECT * FROM cursos");
+                                if($Read->getResult()) {
+                                    foreach($Read->getResult() as $Cursos) {
+                                        ?>
+                                        <tr>
+                                    <td>
+                                        <a href="/" class="user-link"><?= $Cursos['curso_titulo'] ?></a>
+                                        <span class="user-subhead"></span>
+                                    </td>
+                                    <td>
+                                        <?= $Cursos['curso_create_date'] ?>  
+                                    </td>
+                                    
+                                    <td>
+                                        <a href="/"><?= $Cursos['curso_descricao'] ?></a>
+                                    </td>
+                                    <td>
+                                        <a href="/" class="table-link danger">
+                                            <span class="fa-stack">
+                                                <i class="fa fa-square fa-stack-2x"></i>
+                                                <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                            </span>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <?php
+                                    }
+                                } else {
+                                    Error("Ainda não existem usuários!");
+                                }   
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>                
+                </div>
             </div>
+        </div>  
         </div>
-    </div>  
-    </div>
-    <!-- Fim da div container -->
-    </div>
+        <!-- Fim da div container -->
+        </div>
 <?php
 echo $Component->getFooterDashboard();
 ?>
