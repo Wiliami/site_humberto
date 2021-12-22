ed0c31cd384dd3dede1fa7a720c057258c7420c4
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-22 10:29:20 -0400

    arrumando a pagina de nivel de usuario

--- views/painel/dashboard.php
+++ views/painel/dashboard.php
@@ -2,6 +2,6 @@
 $User = new User();
 $User->verifyExistLoginUser();
 $Component = new Component();
+echo $Component->getHeadHtmlDashboardExample();
 echo $Component->getMenuAndSideBarDashboard();
-echo $Component->getFooterDashboard();
-?>
+echo $Component->getFooterDashboard(); 
