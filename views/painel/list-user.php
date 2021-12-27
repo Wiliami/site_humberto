c433f4325063b2550d5f1e086082efd2dd72a74f
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-27 10:27:25 -0400

    arrumando a página de lista de usuários

--- views/painel/list-user.php
+++ views/painel/list-user.php
@@ -4,168 +4,88 @@ $User->verifyExistLoginUser();
 $Component = new Component();
 echo $Component->getHeadHtmlDashboard();
 echo $Component->getMenuSideBarDashboard();
+echo $Component->getMenuOptionsDashboard();
 ?>
-<div class="container-fluid">
-<div class="row">
-    <a href="<?= BASE ?>/painel/nivel-user" class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-primary shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                            Níveis de usuários
-                        </div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-    <a href="#" class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                            Deletar Usuários</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-    <a href="#" class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Alterar 
-                        </div>
-                        <div class="row no-gutters align-items-center">
-                            <div class="col-auto">
-                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"></div>
-                            </div>
-                            <div class="col">
-                                <div class="progress progress-sm mr-2">
-                                    <div class="progress-bar bg-info" role="progressbar"
-                                        style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                        aria-valuemax="100"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-    <a href="#" class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-warning shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                            Contato</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-comments fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-</div>
-</div> 
 <div class="container">
     <div class="row">
         <div class="col-lg-12">
-        <div class="main-box clearfix">
-            <div class="table-responsive">
-                <table class="table user-list">
-                <h2>Lista de usuários</h2>
-                    <thead>
-                        <tr>
-                            <th><span>Usuário</span></th>
-                            <th><span>Criado</span></th>
-                            <th class="text-center"><span>Status</span></th>
-                            <th><span>E-mail</span></th>
-                            <th>&nbsp;</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $Read = new Read();
-                        $Read->FullRead("SELECT * FROM users");
-                        if($Read->getResult()) {
-                            foreach($Read->getResult() as $User) {
-                                ?>
-                                <tr>
-                            <td>
-                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar" style="width: 50px; height: 50px">
-                                <a href="<?= BASE ?>/painel/list-user" class="user-link"><?= $User['user_name'] ?></a>
+            <div class="main-box clearfix">
+                <div class="table-responsive">
+                    <table class="table user-list">
+                    <h2>Lista de usuários</h2>
+                        <thead>
+                            <tr>
+                                <th><span>Usuário</span></th>
+                                <th><span>Criado</span></th>
+                                <th class="text-center"><span>Status</span></th>
+                                <th><span>E-mail</span></th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $Read = new Read();
+                            $Read->FullRead("SELECT * FROM users");
+                            if($Read->getResult()) {
+                                foreach($Read->getResult() as $User) {
+                                    ?>
+                                    <tr>
+                                <td>
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar" style="width: 50px; height: 50px">
+                                    <a href="<?= BASE ?>/painel/list-user" class="user-link"><?= $User['user_name'] ?></a>
 
-                    
-                        <!-- Nível de usuário -->
-                        <?php
-                        $Read->FullRead('SELECT * FROM users_levels WHERE level_id = :li', "li={$User['level_id']}");
-                        if($Read->getResult()) {
-                            foreach($Read->getResult() as $User) {
-                                ?>
-                            <span class="user-subhead"><?= $User['level_desc'] ?></span>                                   
-                        <?php
+                        
+                            <!-- Nível de usuário -->
+                            <?php
+                            $Read->FullRead('SELECT * FROM users_levels WHERE level_id = :li', "li={$User['level_id']}");
+                            if($Read->getResult()) {
+                                foreach($Read->getResult() as $User) {
+                                    ?>
+                                <span class="user-subhead"><?= $User['level_desc'] ?></span>                                   
+                            <?php }
                             }
-                        }
-                        ?> 
+                            ?> 
 
-                            </td>
-                            <td>
-                                <?= $User['user_create_date'] ?>  
-                            </td>
-                            <td class="text-center">
-                                <span class="label label-default">Situação</span>
-                            </td>
-                            <td>
-                                <a href="<?= BASE ?>/"><?= $User['user_email']?></a>
-                            </td>
-                            <td>
-                                <a href="<?= BASE ?>/" class="table-link">
-                                    <span class="fa-stack">
-                                        <i class="fa fa-square fa-stack-2x"></i>
-                                        <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                                <a href="<?= BASE ?>/" class="table-link">
-                                    <span class="fa-stack">
-                                        <i class="fa fa-square fa-stack-2x"></i>
-                                        <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                                <a href="<?= BASE ?>/" class="table-link danger">
-                                    <span class="fa-stack">
-                                        <i class="fa fa-square fa-stack-2x"></i>
-                                        <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </td>
-                        </tr>
-                        <?php }
-                        } else {
-                            Error("Ainda não existem usuários!");
-                        }   
-                        ?>
-                    </tbody>
-                </table>
-            </div>                
-        </div>
+                                </td>
+                                <td>
+                                    <?= $User['user_create_date'] ?>  
+                                </td>
+                                <td class="text-center">
+                                    <span class="label label-default">Situação</span>
+                                </td>
+                                <td>
+                                    <a href="<?= BASE ?>/"><?= $User['user_email']?></a>
+                                </td>
+                                <td>
+                                    <a href="<?= BASE ?>/" class="table-link">
+                                        <span class="fa-stack">
+                                            <i class="fa fa-square fa-stack-2x"></i>
+                                            <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </a>
+                                    <a href="<?= BASE ?>/" class="table-link">
+                                        <span class="fa-stack">
+                                            <i class="fa fa-square fa-stack-2x"></i>
+                                            <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </a>
+                                    <a href="<?= BASE ?>/" class="table-link danger">
+                                        <span class="fa-stack">
+                                            <i class="fa fa-square fa-stack-2x"></i>
+                                            <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </a>
+                                </td>
+                            </tr>
+                            <?php }
+                            } else {
+                                Error("Ainda não existem usuários!");
+                            }   
+                            ?>
+                        </tbody>
+                    </table>
+                </div>                
+            </div>
         </div>
     </div>  
 </div>
