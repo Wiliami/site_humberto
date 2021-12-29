e0a3208f2327b4e0a6840857738ad90b350f74ec
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2021-12-29 17:15:14 -0400

    arrumando as paginas dando vida a tabela

--- views/painel/profile/suporte.php
+++ views/painel/profile/suporte.php
@@ -7,7 +7,6 @@ echo $Component->getMenuSideBarDashboard();
 ?>
 <!-- Conteúdo que vai ser exibido -->
 <div class="container-fluid">
-    <!-- ***Page Heading*** -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Suporte</h1>
     </div>
