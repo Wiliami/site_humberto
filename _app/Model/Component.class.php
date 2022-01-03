2d5472da876746f88564ac1fa99eb8463c1cee7d
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-03 12:02:07 -0400

    arrumando a datatables

--- _app/Model/Component.class.php
+++ _app/Model/Component.class.php
@@ -276,11 +276,10 @@ class Component {
                 <link href="' . BASE . '/res/site/css/all.min.css" rel="stylesheet" type="text/css">
                 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
                 <link href="' . BASE . '/res/site/css/sb-admin-2.min.css" rel="stylesheet">
-                <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.3/datatables.min.css"/>
 
                 <!-- DataTables -->
-                <link href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" rel="stylesheet">
-                
+                <link href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+
                 <!-- Estiliza os ícones da dashboard -->
                 <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
             </head> 
