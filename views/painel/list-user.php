d089f409b097356398b834d85e2cb4099bec026c
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-04 10:53:06 -0400

    adicionando a config da dataTables

--- views/painel/list-user.php
+++ views/painel/list-user.php
@@ -70,7 +70,7 @@ echo $Component->getMenuSideBarDashboard();
                                             <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
                                         </span>
                                     </a>
-                                    <a href="<?= BASE ?>/" class="table-link danger" title="Excluir <?= $User['user_name'] ?>" data-toggle="modal" data-target="#deleteModal">
+                                    <a href="<?= BASE ?>/" class="table-link" title="Excluir <?= $User['user_name'] ?>" data-toggle="modal" data-target="#deleteModal">
                                         <span class="fa-stack">
                                             <i class="fa fa-square fa-stack-2x"></i>
                                             <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
