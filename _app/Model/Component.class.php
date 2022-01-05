5e87c7bbcb25c5917067bf88ef9e7ca8a594c243
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-05 17:30:53 -0400

    arrumando a pagina de aulas

--- _app/Model/Component.class.php
+++ _app/Model/Component.class.php
@@ -271,10 +271,10 @@ class Component {
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                 <meta name="description" content="">
                 <meta name="author" content="">
-                <title>' . $title . '</title>
-                <link href="' . BASE . '/res/site/css/all.min.css" rel="stylesheet" type="text/css">
+                <title>'. $title .'</title>
+                <link href="'. BASE .'/res/site/css/all.min.css" rel="stylesheet" type="text/css">
                 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-                <link href="' . BASE . '/res/site/css/sb-admin-2.min.css" rel="stylesheet">
+                <link href="'. BASE .'/res/site/css/sb-admin-2.min.css" rel="stylesheet">
                 <!-- Estiliza os ícones da dashboard -->
                 <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
             </head> 
@@ -783,10 +783,7 @@ class Component {
         <script src="'. BASE .'/res/site/js/jquery.easing.min.js"></script>
         <script src="'. BASE .'/res/site/js/sb-admin-2.min.js"></script> 
     </body>
-</html>
-
-
-        
+</html>        
         ';
     }
    
