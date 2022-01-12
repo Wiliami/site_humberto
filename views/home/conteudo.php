635777af33cea9d505570ee5aea3282e3473b991
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-12 16:42:39 -0400

    finalizadno a página de conteúdos

--- views/home/conteudo.php
+++ views/home/conteudo.php
@@ -88,28 +88,7 @@ echo $Component->getMenu();
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/6n58QTH3xLc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                    </div>
                 </div>
             </div>
-          </div>
-          <!--Footer-->
-          <div class="modal-footer justify-content-center">
-            <span class="mr-4">Spread the word!</span>
-            <a type="button" class="btn-floating btn-sm btn-fb"><i class="fab fa-facebook-f"></i></a>
-            <!--Twitter-->
-            <a type="button" class="btn-floating btn-sm btn-tw"><i class="fab fa-twitter"></i></a>
-            <!--Google +-->
-            <a type="button" class="btn-floating btn-sm btn-gplus"><i class="fab fa-google-plus-g"></i></a>
-            <!--Linkedin-->
-            <a type="button" class="btn-floating btn-sm btn-ins"><i class="fab fa-linkedin-in"></i></a>
-            <button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
-          </div>
         </div>
-        <!--/.Content-->
-      </div>
     </div>
-    <!--Modal: Name-->
-    <a><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-3.jpg" alt="video"
-        data-toggle="modal" data-target="#modal4"></a>
-  </div>
-</div>
-<!-- Grid row -->
 </section>
 <?= $Component->getFooterHome(); ?>  
