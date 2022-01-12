27f50cb5d0567d59ca609681e221536d11165fc2
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-12 10:16:28 -0400

    algumas configurações da datatables

--- views/painel/list-user.php
+++ views/painel/list-user.php
@@ -7,103 +7,148 @@ echo $Component->getHeadHtmlDataTable();
 echo $Component->getMenuSideBarDashboard();
 ?>
 <div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="main-box clearfix">
-                <div class="table-responsive">
-                    <table id="table-usuarios" class="table table-striped table-bordered" style="width: 100%;">
-                        <div class="d-sm-flex align-items-center justify-content-start mb-4">
-                            <i class="fas fa-list"></i>
-                            <h1 class="h3 ml-2 mb-0 text-gray-800">Lista de usuários</h1>
-                        </div>
-                        <thead>
-                            <tr>
-                                <th>*</th>
-                                <th>Usuário</th>
-                                <th>Nível</th>
-                                <th>Criado</th>
-                                <th>Status</th>
-                                <th>E-mail</th>
-                                <th>Opções</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $Read = new Read();
-                            $Read->FullRead("SELECT u.*, ul.level_desc
-                                FROM users u
-                                LEFT JOIN users_levels ul ON ul.level_id = u.user_level");
-                                // Primeiro eu tenho o id e o campo da tabela que eu quero 
-                            if($Read->getResult()) {
-                                foreach($Read->getResult() as $User) { 
-                                    ?>
-                            <tr>
-                                <td>
-                                    <?= $Component->getAvatarUser(); ?>
-                                </td>
-                                <td>
-                                    <?= $User['user_name'] ?>
-                                </td>
-                                <td>
-                                    <?= $User['level_desc'] ?>
-                                </td>
-                                <td>
-                                    <?= date('d/m/Y', strtotime($User['user_create_date'])) ?>
-                                </td>
-                                <td>
-                                    <?= $User['user_status'] ?>
-                                </td>
-                                <td>
-                                    <?= $User['user_email'] ?>
-                                </td>
-                                <td>
-                                    <a href="<?= BASE ?>/" class="table-link"
-                                        title="Pesquisar <?= $User['user_name'] ?> ">
-                                        <span class="fa-stack">
-                                            <i class="fa fa-square fa-stack-2x"></i>
-                                            <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                    <a href="<?= BASE ?>/" class="table-link"
-                                        title="Alterar o nível de <?= $User['user_name'] ?> ">
-                                        <span class="fa-stack">
-                                            <i class="fa fa-square fa-stack-2x"></i>
-                                            <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                    <a href="<?= BASE ?>/" class="table-link" title="Excluir <?= $User['user_name'] ?>"
-                                        data-toggle="modal" data-target="#deleteModal">
-                                        <span class="fa-stack">
-                                            <i class="fa fa-square fa-stack-2x"></i>
-                                            <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </td>
-                            </tr>
-                            <?php
-                                }
-                            } else{
-                                Error("Ainda não existem usuários!");
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-dark">Lista de usuários</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="table-usuarios" class="table table-striped table-bordered" style="width: 100%;">
+                    <thead>
+                        <tr>
+                            <th>*</th>
+                            <th>Usuário</th>
+                            <th>Nível</th>
+                            <th>Criado</th>
+                            <th>Status</th>
+                            <th>E-mail</th>
+                            <th>Opções</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $Read = new Read();
+                        $Read->FullRead("SELECT u.*, ul.level_desc
+                            FROM users u
+                            LEFT JOIN users_levels ul ON ul.level_id = u.user_level");
+                            // Primeiro eu tenho o id e o campo da tabela que eu quero 
+                        if($Read->getResult()) {
+                            foreach($Read->getResult() as $User) { 
+                                ?>
+                        <tr>
+                            <td>
+                                <?= $Component->getAvatarUser(); ?>
+                            </td>
+                            <td>
+                                <?= $User['user_name'] ?>
+                            </td>
+                            <td>
+                                <?= $User['level_desc'] ?>
+                            </td>
+                            <td>
+                                <?= date('d/m/Y', strtotime($User['user_create_date'])) ?>
+                            </td>
+                            <td>
+                                <?= $User['user_status'] ?>
+                            </td>
+                            <td>
+                                <?= $User['user_email'] ?>
+                            </td>
+                            <td>
+                                <a href="<?= BASE ?>/" class="table-link"
+                                    title="Pesquisar <?= $User['user_name'] ?> ">
+                                    <span class="fa-stack">
+                                        <i class="fa fa-square fa-stack-2x"></i>
+                                        <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                                <a href="<?= BASE ?>/" class="table-link"
+                                    title="Alterar o nível de <?= $User['user_name'] ?> ">
+                                    <span class="fa-stack">
+                                        <i class="fa fa-square fa-stack-2x"></i>
+                                        <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                                <a href="<?= BASE ?>/" class="table-link" title="Excluir <?= $User['user_name'] ?>"
+                                    data-toggle="modal" data-target="#deleteModal">
+                                    <span class="fa-stack">
+                                        <i class="fa fa-square fa-stack-2x"></i>
+                                        <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                            </td>
+                        </tr>
+                        <?php
                             }
-                            ?>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th>*</th>
-                                <th>Usuário</th>
-                                <th>Nível</th>
-                                <th>Criado</th>
-                                <th>Status</th>
-                                <th>E-mail</th>
-                                <th>Opções</th>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
+                        } else{
+                            Error("Ainda não existem usuários!");
+                        }
+                        ?>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>*</th>
+                            <th>Usuário</th>
+                            <th>Nível</th>
+                            <th>Criado</th>
+                            <th>Status</th>
+                            <th>E-mail</th>
+                            <th>Opções</th>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
         </div>
     </div>
 </div>
+
+
+
+
+
+
+<div class="container-fluid">
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-dark">Nome da tabela</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Age</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Age</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.container-fluid -->
+
+
+
+
+
 <!-- Delete Modal -->
 <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
@@ -136,18 +181,20 @@ echo $Component->getMenuSideBarDashboard();
     </div>
 </div>
 <?= $Component->getFooterDashboard(); ?>
+<script src="<? BASE ?>/src/datatables/jquery.dataTables.min.js"></script>
+<script src="<? BASE ?>/src/datatables/dataTables.bootstrap4.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" language="javascript">
 $(document).ready(function() {
-$("#table-usuarios").DataTable({
+    $("#table-usuarios").DataTable({
         "language": {
             "lengthMenu": "Mostrando _MENU_ registros por página",
             "zeroRecords": "Nenhum registro foi encontrado",
             "info": "Mostrando página _PAGE_ de _PAGES_ registros",
             "infoEmpty": "Nenhum registro foi encontrado",
             "infoFiltered": "(filtrado de _MAX_ registros no total)"
-        }      
+        }
     });
 });
 </script>
--- views/painel/lista-usuario.php
+++ views/painel/lista-usuario.php
@@ -3,34 +3,31 @@ $User = new User();
 $User->verifyExistLoginUser();
 $Component = new Component();
 echo $Component->getHeadHtmlDashboard();
+echo $Component->getHeadHtmlDataTable();
 echo $Component->getMenuSideBarDashboard();
 //$ListaUsuario = $_GET['page'];    
 ?>
-<div class="container-fluid">
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-400">Lista de usuários</h1>
-    </div>
-        <p>Lista</p>
-</div>
 <div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="main-box clearfix">
-                <div class="table-responsive">
-                    <table class="table user-list">
-                        <thead>
-                            <tr>
-                                <th><span class="btn btn-warning mb-2">#</span></th>
-                                <th><span class="btn btn-warning mb-2">Usuário</span></th>
-                                <th><span class="btn btn-warning mb-2">Nível</span></th>
-                                <th><span class="btn btn-warning mb-2">Criado</span></th>
-                                <th class="text-center"><span class="btn btn-warning mb-2">Status</span></th>
-                                <th><span class="btn btn-warning mb-2">E-mail</span></th>
-                                <th>&nbsp;</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-dark">Todos os usuários</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="table-listar-usuarios" class="table table-striped table-bordered" style="width: 100%;">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Usuário</th>
+                            <th>Nível</th>
+                            <th>Criado</th>
+                            <th>Status</th>
+                            <th>E-mail</th>
+                            <th>Opções</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
                             $Read = new Read();
                             $Read->FullRead("SELECT u.*, ul.level_desc 
                                     FROM users u 
@@ -38,68 +35,87 @@ echo $Component->getMenuSideBarDashboard();
                             if($Read->getResult()) {
                                 foreach($Read->getResult() as $User) {                    
                                     ?>
-                                    <tr>
-                                        <td>
-                                            <span><?= $Component->getAvatarUser(); ?></span>
-                                        </td>
-                                        <td>
-                                            <span class="d-block user-link btn btn-light mb-2"><?= $User['user_name'] ?></span>
-                                        </td>
-                                        <td>
-                                            <span class="btn btn-light mb-2"><?= $User['level_desc'] ?></td></span>
-                                        <td>
-                                            <span class="btn btn-light mb-2"><?= date('d/m/Y', strtotime($User['user_create_date'])) ?></span>
-                                        </td>
-                                        <td>
-                                            <span class="btn btn-light mb-2">Situação</span>
-                                        </td>
-                                        <td>
-                                            <span class="btn btn-light mb-2"><?= $User['user_email'] ?></span>
-                                        </td>
-                                        <td>
-                                            <a href="<?= BASE ?>/painel/courses/list" class="table-link" title="Pesquisar <?= $User['user_name'] ?>">
-                                                <span class="fa-stack">
-                                                    <i class="fa fa-square fa-stack-2x"></i>
-                                                    <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                            </a>
-                                            <a href="<?= BASE ?>/painel/courses/update" class="table-link" title="Alterar nível de <?= $User['user_name'] ?>">
-                                                <span class="fa-stack">
-                                                    <i class="fa fa-square fa-stack-2x"></i>
-                                                    <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                            </a>
-                                            <a href="<?= BASE ?>/painel/courses/delete" class="table-link danger" title="Excluir <?= $User['user_name'] ?>">
-                                                <span class="fa-stack">
-                                                    <i class="fa fa-square fa-stack-2x"></i>
-                                                    <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    <?php 
-                                }
-                            } else {
-                                Error("Ainda não existem usuários!");
-                            }   
-                            ?>
-                        </tbody>
-                    </table>
-                </div>                
+                        <tr>
+                            <td>
+                                <span><?= $Component->getAvatarUser(); ?></span>
+                            </td>
+                            <td>
+                                <span><?= $User['user_name'] ?></span>
+                            </td>
+                            <td>
+                                <span><?= $User['level_desc'] ?>
+                            </td></span>
+                            <td>
+                                <span><?= date('d/m/Y', strtotime($User['user_create_date'])) ?></span>
+                            </td>
+                            <td>
+                                <span>Situação</span>
+                            </td>
+                            <td>
+                                <span><?= $User['user_email'] ?></span>
+                            </td>
+                            <td>
+                                <a href="<?= BASE ?>/painel/courses/list" class="table-link"
+                                    title="Pesquisar <?= $User['user_name'] ?>">
+                                    <span class="fa-stack">
+                                        <i class="fa fa-square fa-stack-2x"></i>
+                                        <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                                <a href="<?= BASE ?>/painel/courses/update" class="table-link"
+                                    title="Alterar nível de <?= $User['user_name'] ?>">
+                                    <span class="fa-stack">
+                                        <i class="fa fa-square fa-stack-2x"></i>
+                                        <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                                <a href="<?= BASE ?>#" class="table-link danger"
+                                    title="Excluir <?= $User['user_name'] ?>">
+                                    <span class="fa-stack">
+                                        <i class="fa fa-square fa-stack-2x"></i>
+                                        <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                            </td>
+                        </tr>
+                        <?php 
+                            }
+                        } else {
+                            Error("Ainda não existem usuários!");
+                        }   
+                        ?>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>*</th>
+                            <th>Usuário</th>
+                            <th>Nível</th>
+                            <th>Criado</th>
+                            <th>Status</th>
+                            <th>E-mail</th>
+                            <th>Opções</th>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
         </div>
-    </div>  
-    <?php
-    // Qual a tabela que eu quero ler
-    // Tabela de usuários
-    $Read->FullRead("SELECT * FROM users WHERE LIMIT 1, 10");
-    if($Read->getResult()) {
-        foreach($Read->getResult() as $ListUser) {
-            ?>
-            <a href="<?= BASE ?>/painel/lista-usuario" class="d-sm-flex align-items-center justify-content-center btn btn-success mb-2" title="Carregar mais usuários">Carregar mais...</a>
-    <?php
-        }
-    }   
-    ?>
+    </div>
 </div>
 <?= $Component->getFooterDashboard(); ?>
+<script src="<? BASE ?>/src/datatables/jquery.dataTables.min.js"></script>
+<script src="<? BASE ?>/src/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" language="javascript">
+$(document).ready(function() {
+    $("#table-listar-usuarios").DataTable({
+        "language": {
+            "lengthMenu": "Mostrando _MENU_ registros por página",
+            "zeroRecords": "Nenhum registro foi encontrado",
+            "info": "Mostrando página _PAGE_ de _PAGES_ registros",
+            "infoEmpty": "Nenhum registro foi encontrado",
+            "infoFiltered": "(filtrado de _MAX_ registros no total)"
+        }
+    });
+});
+</script>
