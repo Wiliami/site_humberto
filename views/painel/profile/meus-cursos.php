6165c784d8ea731db262664f2fe25f350ee4fac8
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-13 17:42:24 -0400

    arrumando a página de meus cursos

--- views/painel/profile/meus-cursos.php
+++ views/painel/profile/meus-cursos.php
@@ -5,44 +5,200 @@ $Component = new Component();
 echo $Component->getHeadHtmlDashboard();
 echo $Component->getMenuSideBarDashboard();
 ?>
+
+
+<?php
+$Read = new Read();
+$Read->FullRead("SELECT * FROM cursos");
+if($Read->getResult()) {
+    foreach($Read->getResult() as $Cursos) {
+        ?>
+<?php
+    }
+} else {
+    Error("Ainda não exite cursos cadastrados!");
+}
+?>
+
+
 <div class="container-fluid">
-    <div class="d-sm-flex align-items-center justify-content-start mb-4">
+    <div class="d-sm-flex align-items-center justify-content-start mb-4 d-block">
         <h1 class="h3 mb-0 text-gray-800">Olá, <?= $_SESSION['login']['user_name']?></h1>
     </div>
 </div>
 <div class="row gx-5 container">
     <div class="col-lg-4 mb-5">
         <div class="card h-100 shadow border-0">
-            <!-- <img src="<?= BASE ?>/src/images/page-sobre.jpg" alt="imagem de fundo"/> -->
+            <img src="<?= BASE ?>/src/images/page-sobre.jpg" alt="imagem de fundo"/>
             <div class="card-body p-4">
                 <div class="badge bg-success bg-gradient rounded-pill mb-2 text-white">Curso</div>
                 <a class="text-decoration-none link-dark stretched-link" href="<?= BASE ?>/painel/aulas">
-                <?php
-                $Read = new Read();
-                $Read->FullRead("SELECT * FROM cursos");
-                if($Read->getResult()) {
-                    foreach($Read->getResult() as $Cursos) {
-                        ?>
-                    <h5 class="card-title mb-3"><?= $Cursos['curso_titulo'] ?></h5>
+                    <h5 class="card-title mb-3">curso_titulo</h5>
                 </a>
-                <p class="card-text mb-0"><?= $Cursos['curso_descricao'] ?></p>
+                <p class="card-text mb-0">curso_descricao</p>
                 <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
                     <div class="d-flex align-items-end justify-content-between">
                         <div class="d-flex align-items-center">
                             <!-- <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." /> -->
                             <div class="small">
-                                <div class="fw-bold"><?= $Cursos['curso_categoria'] ?></div>
-                                <div class="text-muted"><?= date('d/m/Y', strtotime($Cursos['curso_create_date'])) ?></div>
+                                <div class="fw-bold">curso_categoria</div>
+                                <div class="text-muted">curso_create_date</div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <?php
-                    }
-                } else {
-                    Error("Ainda não exite cursos cadastrados!");
-                }
-                ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 mb-5">
+        <div class="card h-100 shadow border-0">
+            <img src="<?= BASE ?>/src/images/page-sobre.jpg" alt="imagem de fundo"/>
+            <div class="card-body p-4">
+                <div class="badge bg-success bg-gradient rounded-pill mb-2 text-white">Curso</div>
+                <a class="text-decoration-none link-dark stretched-link" href="<?= BASE ?>/painel/aulas">
+                    <h5 class="card-title mb-3">curso_titulo</h5>
+                </a>
+                <p class="card-text mb-0">curso_descricao</p>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
+                            <div class="small">
+                                <div class="fw-bold">curso_categoria</div>
+                                <div class="text-muted">curso_create_date</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-lg-4 mb-5">
+        <div class="card h-100 shadow border-0">
+            <img src="<?= BASE ?>/src/images/page-sobre.jpg" alt="imagem de fundo"/>
+            <div class="card-body p-4">
+                <div class="badge bg-success bg-gradient rounded-pill mb-2 text-white">Curso</div>
+                <a class="text-decoration-none link-dark stretched-link" href="<?= BASE ?>/painel/aulas">
+                  
+                    <h5 class="card-title mb-3">curso_titulo</h5>
+                </a>
+                <p class="card-text mb-0">curso_descricao</p>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <!-- <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." /> -->
+                            <div class="small">
+                                <div class="fw-bold">curso_categoria</div>
+                                <div class="text-muted">curso_create_date</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-lg-4 mb-5">
+        <div class="card h-100 shadow border-0">
+            <img src="<?= BASE ?>/src/images/page-sobre.jpg" alt="imagem de fundo"/>
+            <div class="card-body p-4">
+                <div class="badge bg-success bg-gradient rounded-pill mb-2 text-white">Curso</div>
+                <a class="text-decoration-none link-dark stretched-link" href="<?= BASE ?>/painel/aulas">
+                  
+                    <h5 class="card-title mb-3">curso_titulo</h5>
+                </a>
+                <p class="card-text mb-0">curso_descricao</p>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <!-- <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." /> -->
+                            <div class="small">
+                                <div class="fw-bold">curso_categoria</div>
+                                <div class="text-muted">curso_create_date</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-lg-4 mb-5">
+        <div class="card h-100 shadow border-0">
+            <img src="<?= BASE ?>/src/images/page-sobre.jpg" alt="imagem de fundo"/>
+            <div class="card-body p-4">
+                <div class="badge bg-success bg-gradient rounded-pill mb-2 text-white">Curso</div>
+                <a class="text-decoration-none link-dark stretched-link" href="<?= BASE ?>/painel/aulas">
+                  
+                    <h5 class="card-title mb-3">curso_titulo</h5>
+                </a>
+                <p class="card-text mb-0">curso_descricao</p>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <!-- <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." /> -->
+                            <div class="small">
+                                <div class="fw-bold">curso_categoria</div>
+                                <div class="text-muted">curso_create_date</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 mb-5">
+        <div class="card h-100 shadow border-0">
+            <img src="<?= BASE ?>/src/images/page-sobre.jpg" alt="imagem de fundo"/>
+            <div class="card-body p-4">
+                <div class="badge bg-success bg-gradient rounded-pill mb-2 text-white">Curso</div>
+                <a class="text-decoration-none link-dark stretched-link" href="<?= BASE ?>/painel/aulas">
+                    <h5 class="card-title mb-3">curso_titulo</h5>
+                </a>
+                <p class="card-text mb-0">curso_descricao</p>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <!-- <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." /> -->
+                            <div class="small">
+                                <div class="fw-bold">curso_categoria</div>
+                                <div class="text-muted">curso_create_date</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-lg-4 mb-5">
+        <div class="card h-100 shadow border-0">
+            <img src="<?= BASE ?>/src/images/page-sobre.jpg" alt="imagem de fundo"/>
+            <div class="card-body p-4">
+                <div class="badge bg-success bg-gradient rounded-pill mb-2 text-white">Curso</div>
+                <a class="text-decoration-none link-dark stretched-link" href="<?= BASE ?>/painel/aulas">
+                  
+                    <h5 class="card-title mb-3">curso_titulo</h5>
+                </a>
+                <p class="card-text mb-0">curso_descricao</p>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <!-- <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." /> -->
+                            <div class="small">
+                                <div class="fw-bold">curso_categoria</div>
+                                <div class="text-muted">curso_create_date</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
