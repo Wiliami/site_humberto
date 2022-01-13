49e9b49686fd48d0b62728c1e3d266d47518316b
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-13 09:49:32 -0400

    arrumando a pagina de conteudo

--- views/home/conteudo.php
+++ views/home/conteudo.php
@@ -4,20 +4,22 @@ echo $Component->getHeadHtmlHome();
 echo $Component->getMenu();
 ?>
 <header class="header-2">
-    <div class="page-header min-vh-100 relative" style="background-image: url('<?= BASE ?>/src/images/page-youtube1.jpg')">
+    <div class="page-header min-vh-100 relative"
+        style="background-image: url('<?= BASE ?>/src/images/page-youtube1.jpg')">
         <span class="mask bg-gradient-secondary opacity-4"></span>
         <div class="container">
             <div class="row">
                 <div class="my-5 text-center text-xl-start">
-                    <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                        <img src="<?= BASE ?>/src/images/youtube-white.png" alt="youtube logo" style="height: 200px; width: 350px;">
-                    </div>
+                    <img src="<?= BASE ?>/src/images/youtube-white.png"  class="d-sm-flex justify-content-sm-center justify-content-xl-start" alt="youtube logo"
+                        style="height: 200px; width: 300px;">
                     <h1 class="display-5 fw-bolder text-white mb-2">Junte-se a nós!</h1>
-                    <p class="lead text-white mt-3">Se inscreva em nosso canal <br> do youtube para acompanhar <br> novos conteúdos!</p>
+                    <p class="lead text-white mt-3">Se inscreva em nosso canal <br> do youtube para acompanhar <br>
+                    novos conteúdos!</p>
                 </div>
                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="https://www.youtube.com/channel/UClFMwemd5j7EsXlV-gTw5Xg">INSCREVA-SE NO CANAL</a>
-                </div>  
+                    <a class="btn btn-primary btn-lg px-4 me-sm-3"
+                        href="https://www.youtube.com/channel/UClFMwemd5j7EsXlV-gTw5Xg">INSCREVA-SE NO CANAL</a>
+                </div>
             </div>
         </div>
     </div>
@@ -25,24 +27,25 @@ echo $Component->getMenu();
 <section class="py-sm-7 py-5 position-relative">
     <div class="container">
         <div class="row">
-        <div class="col-12 mx-auto text-center text-xl-start">
-            <div class="mt-n8 mt-md-n9 text-start">
-                Produção de conteúdos para o Youtube
-            </div>
-            <div class="row py-5">
-                <div class="col-lg-7 col-md-7 z-index-2 position-relative px-md-2 px-sm-5 mx-auto mt-4">
-                    <div class="d-flex justify-content-between align-items-center mb-2">
-                        <!-- Title -->
-                        <h3 class="mb-0">Conteúdo através da mídia</h3>
+            <div class="col-12 mx-auto text-center text-xl-start">
+                <div class="mt-n8 mt-md-n9 text-start">
+                    Produção de conteúdos para o Youtube
+                </div>
+                <div class="row py-5">
+                    <div class="col-lg-7 col-md-7 z-index-2 position-relative px-md-2 px-sm-5 mx-auto mt-4">
+                        <div class="d-flex justify-content-between align-items-center mb-2">
+                            <!-- Title -->
+                            <h3 class="mb-0">Conteúdo através da mídia</h3>
                         </div>
                         <p class="text-lg mb-0 text-start">
-                        Acreditamos que as mídias sociais têm o poder de impulsionar conteúdos relevantes para as
-                        pessoas. Então, usamos esta ferramenta de divulgação de conteúdos, o youtube, para criar e postar conteúdos de qualidade.</p>
+                            Acreditamos que as mídias sociais têm o poder de impulsionar conteúdos relevantes para as
+                            pessoas. Então, usamos esta ferramenta de divulgação de conteúdos, o youtube, para criar e
+                            postar conteúdos de qualidade.</p>
                     </div>
                 </div>
             </div>
         </div>
-        </div>
+    </div>
     </div>
 </section>
 
@@ -55,42 +58,63 @@ echo $Component->getMenu();
         <div class="row gx-5">
             <div class="col-lg-4 mb-5">
                 <div class="d-flex align-items-center">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/NqS42PgzG00" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/NqS42PgzG00"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
                 </div>
             </div>
             <div class="col-lg-4 mb-5">
                 <div class="card h-100 shadow border-0">
                     <div class="d-flex align-items-center">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/RIDwf9LZP0M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/RIDwf9LZP0M"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 mb-5">
                 <div class="card h-100 shadow border-0">
                     <div class="d-flex align-items-center">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/YJswtuiYLNc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div> 
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/YJswtuiYLNc"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-4 mb-5">
                 <div class="card h-100 shadow border-0">
                     <div class="d-flex align-items-center">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/x15_wgECOSg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                    </div>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/x15_wgECOSg"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-4 mb-5">
                 <div class="card h-100 shadow border-0">
                     <div class="d-flex align-items-center">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/uhuDxPtoYyo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                    </div>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/uhuDxPtoYyo"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-4 mb-5">
                 <div class="card h-100 shadow border-0">
                     <div class="d-flex align-items-center">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/6n58QTH3xLc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                    </div>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/6n58QTH3xLc"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<?= $Component->getFooterHome(); ?>  
+<?= $Component->getFooterHome(); ?>
