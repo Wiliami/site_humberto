b2cf89ac38b6f113bd71e8ea15965578f22d54c7
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-14 14:01:18 -0400

    arrumando a página de aulas

--- views/painel/courses/create.php
+++ views/painel/courses/create.php
@@ -6,15 +6,16 @@ $Component = new Component();
 echo $Component->getHeadHtmlDashboard();
 echo $Component->getMenuSideBarDashboard();
 ?>
-<div class="container-fluid">
+<div class="container">
     <div class="d-sm-flex align-items-center justify-content-start mb-4">
         <i class="fas fa-layer-plus"></i>
         <h1 class="h3 mb-0 text-gray-800 ml-2">Create | Cadastrar cursos</h1>
     </div>
     <p class="ml-2">Página de cadastro de cursos</p>
-</div>
-<form method="post">
-    <div class="px-4 py-sm-5 py-3">
+
+    <form method="post">
+        <input type="submit" class="btn btn-success mb-2 ml-4" name="register_category" value="Cadastrar categoria">
+        <div class="px-4 py-sm-5 py-3">
         <?php
         $Post = filter_input_array(INPUT_POST, FILTER_DEFAULT);
         if(!empty($Post['register_course'])) {
@@ -33,34 +34,38 @@ echo $Component->getMenuSideBarDashboard();
             }   
         }
         ?>
-    </div>
-    <div class="form-group row ml-4">
-        <label for="inputPassword" class="col-sm-1 col-form-label btn btn-warning mb-2">Nome</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" placeholder="Nome do curso" name="title" id="inputPassword" value="<?= isset($Post['title'])? $Post['title']: '' ?>">
         </div>
-    </div>
-    <div class="form-group row ml-4">
-        <label for="inputPassword" class="col-sm-1 col-form-label btn btn-warning mb-2">Descrição</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" placeholder="Descrição do curso" name="description" id="inputPassword" value="<?= isset($Post['description'])? $Post['description']: '' ?>">     
+        <div class="form-group row ml-4">
+            <label for="inputPassword" class="col-sm-1 col-form-label btn btn-warning mb-2">Nome</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" placeholder="Nome do curso" name="title" id="inputPassword"
+                    value="<?= isset($Post['title'])? $Post['title']: '' ?>">
+            </div>
         </div>
-    </div>
-    <div class="form-group row ml-4">
-        <label for="inputPassword" class="col-sm-1 col-form-label btn btn-warning mb-2">Categoria</label>
-        <div class="col-sm-10">
-            <!-- <input type="text" class="form-control exampleFormControlSelect1" id="exampleFormControlSelect1" placeholder="Selecione uma categoria" name="category" value="<?= isset($Post['category'])? $Post['category']: '' ?>"> -->
-            <select class="form-control" id="exampleFormControlSelect1" name="category">
-                <option>Educação</option>
-                <option>Motivacional</option>
-                <option>Coaching</option>
-                <option>Economia</option>
-                <option>Casamento</option>
-                <option>Filmes e séries</option>
-                <option>Cultura</option>
-            </select>
+        <div class="form-group row ml-4">
+            <label for="inputPassword" class="col-sm-1 col-form-label btn btn-warning mb-2">Descrição</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" placeholder="Descrição do curso" name="description"
+                    id="inputPassword" value="<?= isset($Post['description'])? $Post['description']: '' ?>">
+            </div>
         </div>
-    </div>
-    <input type="submit" class="btn btn-success mb-2 ml-4" name="register_course" value="Cadastrar curso">
-</form> 
+        <div class="form-group row ml-4">
+            <label for="inputPassword" class="col-sm-1 col-form-label btn btn-warning mb-2">Categoria</label>
+            <div class="col-sm-10">
+                <!-- <input type="text" class="form-control exampleFormControlSelect1" id="exampleFormControlSelect1" placeholder="Selecione uma categoria" name="category" value="<?= isset($Post['category'])? $Post['category']: '' ?>"> -->
+                <select class="form-control" id="exampleFormControlSelect1" name="category">
+                    <option>Educação</option>
+                    <option>Motivacional</option>
+                    <option>Coaching</option>
+                    <option>Economia</option>
+                    <option>Casamento</option>
+                    <option>Filmes e séries</option>
+                    <option>Cultura</option>
+                </select>
+            </div>
+        </div>
+        <input type="submit" class="btn btn-success mb-2 ml-4" name="register_course" value="Cadastrar curso">
+    </form>
+</div>
+
 <?= $Component->getFooterDashboard(); ?>
