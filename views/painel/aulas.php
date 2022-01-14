a69f56dc4febdb62d22067e13895a477a8d597bc
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-01-14 09:14:13 -0400

    arrumando a página de aulas

--- views/painel/aulas.php
+++ views/painel/aulas.php
@@ -11,7 +11,7 @@ $Component = new Component();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>Página | Aulas</title>
+        <title>Painel | Aulas</title>
         <link href="<?= BASE ?>/res/site/css/sb-admin-2.min.css" rel="stylesheet">
         <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     </head>
