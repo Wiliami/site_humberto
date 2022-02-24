db07b41c148f4b8edd55356d8e5835e6026f2d45
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-02-24 18:17:46 -0400

    adicionando select de cursos e aulas

--- views/painel/matriculas-cursos/create.php
+++ views/painel/matriculas-cursos/create.php
@@ -32,31 +32,30 @@ echo $Component->getMenuDashboard();
         ?>
         <div class="form-group">         
             <label for="exampleInputEmail1">Curso</label>
-            <select class="form-control" name="matriculate_course"
-                value="<?= isset($Post['matriculate_course'])? $Post['matriculate_course']: '' ?>">
-                <?php
-                $Read = new Read();
-                $Read->FullRead("SELECT c.*, u.user_name
-                    FROM cursos c
-                    LEFT JOIN users u ON u.user_id = c.user_id");
-                    if($Read->getResult()) {
-                        echo "<option value=''>selecionar</option>";
-                        foreach($Read->getResult() as $Cursos) {
-                            echo "<option value='{$Cursos['curso_id']}'>{$Cursos['curso_titulo']}</option>";
-                                ?> 
+            <select class="form-control" name="matriculate_course" value="<?= isset($Post['matriculate_course'])? $Post['matriculate_course']: '' ?>">
+            <?php
+            $Read = new Read();
+            $Read->FullRead("SELECT c.*, u.user_name
+                FROM cursos c
+                LEFT JOIN users u ON u.user_id = c.user_id");
+                if($Read->getResult()) {
+                    echo "<option value=''>selecionar</option>";
+                    foreach($Read->getResult() as $Cursos) {
+                        echo "<option value='{$Cursos['curso_id']}'>{$Cursos['curso_titulo']}</option>";
+                            ?> 
             </select>    
             <label for="exampleInputEmail1" class="">Escolher usuário</label>
-            <select class="form-control" name="matriculate_user_course" 
-                value="<?= isset($Post['matriculate_user_course'])? $Post['matriculate_user_course']: '' ?>">
-                <?php
-                            foreach($Read->getResult() as $Users) {
-                                echo "<option value='{$Users['user_id']}'>{$Users['user_name']}</option>";
-                            } 
-                        }
-                    } else {
-                        echo "<option value=''>Não existe lista de cursos ou usuários!</option>";
+            <select class="form-control" name="matriculate_user_course" value="<?= isset($Post['matriculate_user_course'])? $Post['matriculate_user_course']: '' ?>">
+            <?php
+                        echo "<option value=''>selecionar</option>";
+                        foreach($Read->getResult() as $Users) {
+                            echo "<option value='{$Users['user_id']}'>{$Users['user_name']}</option>";
+                        } 
                     }
-                ?>
+                } else {
+                    echo "<option value=''>Não existe lista de cursos ou usuários!</option>";
+                }
+            ?>
             </select>
         </div>
         <input type="submit" class="btn btn-success mb-2" name="register_matriculate_course" value="Matricular">
