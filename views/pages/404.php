7843034478d7155cb8a8c4956cc8d0a82cf38609
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-02-25 10:11:46 -0400

    link css sb-admin-2

--- views/pages/404.php
+++ views/pages/404.php
@@ -9,7 +9,7 @@
         <title>Painel | Página não encontrada!</title>
         <link href="<?= BASE ?>/src/css/all.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-        <link href="<?= BASE ?>/res/site/css/sb-admin-2.min.css" rel="stylesheet">
+        <link href="<?= BASE ?>/res/site/css/sb-admin-2.min.css" rel="stylesheet" />
         <link href="<?= BASE ?>/res/site/css/paper-kit.css?v=2.2.0" rel="stylesheet" />
     </head>
     <body>
