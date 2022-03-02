182aec2a6a041554e09cd311baf20efb2cc4e870
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-03-02 14:28:30 -0400

    exemplo de sidebar update

--- _app/Model/Component.class.php
+++ _app/Model/Component.class.php
@@ -918,16 +918,16 @@ class Component {
         }
 
         public function getSuporteDashboard() {
-        return '
-        <div class="container-fluid">
-            <div class="d-sm-flex align-items-end justify-content-end mb-4">
-                <a href="' . BASE . '/profile/suporte"
-                    class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
-                    <i class="far fa-question-circle text-white-50"></i>
-                    Ajuda
-                </a>
+            return '
+            <div class="container-fluid">
+                <div class="d-sm-flex align-items-end justify-content-end mb-4">
+                    <a href="' . BASE . '/profile/suporte"
+                        class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
+                        <i class="far fa-question-circle text-white-50"></i>
+                        Ajuda
+                    </a>
+                </div>
             </div>
-        </div>
         ';
         }
 
@@ -1183,68 +1183,68 @@ class Component {
                     </li>
                     <li>
                         <div class="iocn-link">
-                        <a href="#">
-                            <i class="bx bx-book-alt" ></i>
-                            <span class="link_name">Posts</span>
-                        </a>
-                        <i class="bx bxs-chevron-down arrow" ></i>
+                            <a href="#">
+                                <i class="bx bx-book-alt" ></i>
+                                <span class="link_name">Posts</span>
+                            </a>
+                            <i class="bx bxs-chevron-down arrow" ></i>
                         </div>
                         <ul class="sub-menu">
-                        <li><a class="link_name" href="#">Posts</a></li>
-                        <li><a href="#">Web Design</a></li>
-                        <li><a href="#">Login Form</a></li>
-                        <li><a href="#">Card Design</a></li>
+                            <li><a class="link_name" href="#">Posts</a></li>
+                            <li><a href="#">Web Design</a></li>
+                            <li><a href="#">Login Form</a></li>
+                            <li><a href="#">Card Design</a></li>
                         </ul>
                     </li>
                     <li>
                         <a href="#">
-                        <i class="bx bx-pie-chart-alt-2" ></i>
-                        <span class="link_name">Analytics</span>
+                            <i class="bx bx-pie-chart-alt-2" ></i>
+                            <span class="link_name">Analytics</span>
                         </a>
                         <ul class="sub-menu blank">
-                        <li><a class="link_name" href="#">Analytics</a></li>
+                            <li><a class="link_name" href="#">Analytics</a></li>
                         </ul>
                     </li>
                     <li>
                         <a href="#">
-                        <i class="bx bx-line-chart" ></i>
-                        <span class="link_name">Chart</span>
+                            <i class="bx bx-line-chart" ></i>
+                            <span class="link_name">Chart</span>
                         </a>
                         <ul class="sub-menu blank">
-                        <li><a class="link_name" href="#">Chart</a></li>
+                            <li><a class="link_name" href="#">Chart</a></li>
                         </ul>
                     </li>
                     <li>
                         <div class="iocn-link">
-                        <a href="#">
-                            <i class="bx bx-plug" ></i>
-                            <span class="link_name">Plugins</span>
-                        </a>
-                        <i class="bx bxs-chevron-down arrow" ></i>
+                            <a href="#">
+                                <i class="bx bx-plug" ></i>
+                                <span class="link_name">Plugins</span>
+                            </a>
+                            <i class="bx bxs-chevron-down arrow" ></i>
                         </div>
                         <ul class="sub-menu">
-                        <li><a class="link_name" href="#">Plugins</a></li>
-                        <li><a href="#">UI Face</a></li>
-                        <li><a href="#">Pigments</a></li>
-                        <li><a href="#">Box Icons</a></li>
+                            <li><a class="link_name" href="#">Plugins</a></li>
+                            <li><a href="#">UI Face</a></li>
+                            <li><a href="#">Pigments</a></li>
+                            <li><a href="#">Box Icons</a></li>
                         </ul>
                     </li>
                     <li>
                         <a href="#">
-                        <i class="bx bx-compass" ></i>
-                        <span class="link_name">Explore</span>
-                        </a>
+                            <i class="bx bx-compass" ></i>
+                            <span class="link_name">Explore</span>
+                            </a>
                         <ul class="sub-menu blank">
-                        <li><a class="link_name" href="#">Explore</a></li>
+                            <li><a class="link_name" href="#">Explore</a></li>
                         </ul>
                     </li>
                     <li>
                         <a href="#">
-                        <i class="bx bx-history"></i>
-                        <span class="link_name">History</span>
+                            <i class="bx bx-history"></i>
+                            <span class="link_name">History</span>
                         </a>
                         <ul class="sub-menu blank">
-                        <li><a class="link_name" href="#">History</a></li>
+                            <li><a class="link_name" href="#">History</a></li>
                         </ul>
                     </li>
                     <li>
