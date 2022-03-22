af8111a08f5d7cd74135dd7ec00c1147ad107ad0
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-03-22 17:38:58 -0400

    atualizando páginas

--- views/painel/profile/courses/meus-cursos.php
+++ views/painel/profile/courses/meus-cursos.php
@@ -19,7 +19,7 @@ $useId = $_SESSION['login']['user_id'];
     <?php
     $Read = new Read();
     $Read->FullRead("SELECT m.*, c.curso_titulo, c.curso_descricao
-            FROM matriculas m 
+            FROM matriculas_cursos m 
             LEFT JOIN users u ON u.user_id = m.user_id
             LEFT JOIN cursos c ON c.curso_id = m.curso_id 
             WHERE m.user_id = :id", "id={$useId}");
@@ -32,16 +32,16 @@ $useId = $_SESSION['login']['user_id'];
             <div class="card-body p-4">
                 <div class="badge bg-success bg-gradient rounded-pill mb-2 text-white">Curso</div>
                 <a class="text-decoration-none link-dark stretched-link" href="<?= BASE ?>/painel/profile/aulas">
-                    <h5 class="card-title mb-3"><?= $Matriculas['curso_titulo'] ?></h5>
+                    <h5 class="card-title mb-3"><?= $Mat['curso_titulo'] ?></h5>
                 </a>
-                <p class="card-text mb-0"><?= $Matriculas['curso_descricao'] ?></p>
+                <p class="card-text mb-0"><?= $Mat['curso_descricao'] ?></p>
                 <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
                     <div class="d-flex align-items-end justify-content-between">
                         <div class="d-flex align-items-center">
                             <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
                             <div class="small">
-                                <div class="fw-bold"><?= number_format($Matriculas['matricula_valor_pago'], 2, ',', '.')  ?></div>
-                                <div class="text-muted"><?= date('d/m/Y', strtotime($Matriculas['matricula_create_date'])) ?></div>
+                                <div class="fw-bold"><?= number_format($Mat['matricula_valor_pago'], 2, ',', '.')  ?></div>
+                                <div class="text-muted"><?= date('d/m/Y', strtotime($Mat['matricula_create_date'])) ?></div>
                             </div>
                         </div>
                     </div>
