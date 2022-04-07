62a03045fec6804b3475ecd16ddd631760b82f7c
Wiliamis Oliveira <67030481+Wiliami@users.noreply.github.com>
2022-04-07 08:52:22 -0400

    arrumando a página

--- views/painel/profile/area-curso.php
+++ views/painel/profile/area-curso.php
@@ -9,6 +9,7 @@ echo $Component->getBlockPageProfile();
     <head>
         <title>Plataforma | Humberto Oliveira</title>
         <meta charset="UTF-8">
+        
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
